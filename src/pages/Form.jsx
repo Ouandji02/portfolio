08781b2c78c53ab61655e664916a07c91d89860f
Thierry Ouandji <thierryouandji@pop-os.localdomain>
2023-01-06 00:26:32 +0100

    modication du toggleTheme

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,5 +1,6 @@
 import { sendForm } from "@emailjs/browser";
 import React from "react";
+import { useState } from "react";
 import {
   FaFacebookF,
   FaGithub,
@@ -11,14 +12,28 @@ import {
 } from "react-icons/fa";
 
 export default function Form() {
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [display, setDisplay] = useState(false);
   const sendMail = (e) => {
+    setLoading(true);
     e.preventDefault();
     sendForm(
       "service_0jvqtmv",
       "template_oyhvf3s",
       e.target,
       "user_xbCxtpUwJQMVgWuGJWxfI"
-    );
+    )
+      .then((emailJSResponse) => {
+        setDisplay(true);
+        setSuccess(true);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setDisplay(true);
+        setSuccess(false);
+        setLoading(false);
+      });
   };
   return (
     <div className="relative top-[150px] bg-white dark:bg-bgdark flex justify-center p-10 rounded shadow-lg max-md:px-5 max-md:flex-wrap max-sm:w-full">
@@ -126,15 +141,15 @@ export default function Form() {
           </div>
           <div className="relative z-0 w-full group my-8">
             <input
-              type="phone"
-              name="phone"
-              id="phone"
+              type="number"
+              name="number"
+              id="number"
               className="block  w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 focus:outline-none focus:ring-0 focus:borer-blue-600 peer"
               placeholder=""
               required
             />
             <label
-              htmlFor="phone"
+              htmlFor="number"
               className="peer-focus:font-medium absolute top-[50px] text-sm text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-0 -z-10 origin-[0] peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-6"
             >
               Phone
@@ -143,8 +158,8 @@ export default function Form() {
           <div className="relative z-0 w-full group my-8">
             <input
               type="text"
-              name="sujet"
-              id="sujet"
+              name="subjet"
+              id="subjet"
               className="block  w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 focus:outline-none focus:ring-0 focus:borer-blue-600 peer"
               placeholder=""
               required
@@ -173,10 +188,54 @@ export default function Form() {
             </label>
           </div>
           <div>
+            {display ? (
+              <div
+                class={
+                  success
+                    ? "p-4 mb-4 text-sm text-green-700 bg-green-100 rounded-lg dark:bg-gray-800 dark:text-green-400"
+                    : "p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-gray-800 dark:text-red-400"
+                }
+                role="alert"
+              >
+                {success ? (
+                  <>
+                    <span class="font-medium">Message de success!</span> Votre
+                    message a été bien envoyé{" "}
+                  </>
+                ) : (
+                  <>
+                    <span class="font-medium">Message d'erreur!</span> Votre
+                    message n'a pas été envoyé
+                  </>
+                )}
+              </div>
+            ) : null}
+
             <button
               type="submit"
-              className="dark:bg-dark border-white px-4 py-2 rounded text-sm mr-10 bg-[#422E7B] text-white hover:bg-white hover:text-[#BC5ABA] hover:border-[#BC5ABA] font-extrabold max-sm:mb-5 max-sm:justify-center transition duration-1000"
+              className="flex items-center dark:bg-dark border-white px-4 py-2 rounded text-sm mr-10 bg-[#422E7B] text-white hover:bg-white hover:text-[#BC5ABA] hover:border-[#BC5ABA] font-extrabold max-sm:mb-5 max-sm:justify-center transition duration-1000"
             >
+              {loading ? (
+                <div role="status">
+                  <svg
+                    aria-hidden="true"
+                    className="w-5 h-5 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-[#BC5ABA]"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="currentColor"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentFill"
+                    />
+                  </svg>
+                  <span className="sr-only">Loading...</span>
+                </div>
+              ) : null}
               Submit
             </button>
           </div>
